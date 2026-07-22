85fda3f4ab6c930099511189c17a6f68043a170c
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2026-07-22 11:44:11 +0100

    Added SuitManager Async Extension
    Testing these has not, well, is not easy full stop. We tried to get a 54H on SUIT to start but it's stuck in some sort of deeply unconfigured state and it's not trivial to recover and it's also not trivial to get the help we need to do so. So, we will try to call these even though we'll get errors back. But anyway, we're merging them in.

---
+++ iOSMcuManagerLibrary/Source/Managers/SuitManager+Async.swift
@@ -0,0 +1,71 @@
+//
+//  SuitManager+Async.swift
+//  iOSMcuManagerLibrary
+//
+//  Created by Dinesh Harjani on 09/07/2026.
+//
+
+import Foundation
+
+// MARK: - SuitManager+Async
+
+@available(iOS 13.0, macCatalyst 13.0, macOS 10.15, *)
+public extension SuitManager {
+    
+    // MARK: listManifest()
+    
+    public func listManifest() async throws -> SuitListResponse {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<SuitListResponse, Error>) in
+            listManifest { response, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+
+                if let response {
+                    continuation.resume(returning: response)
+                } else {
+                    continuation.resume(throwing: McuMgrResponseParseError.invalidPayload)
+                }
+            }
+        }
+    }
+    
+    // MARK: processRecentlyUploadedEnvelope()
+    
+    public func processRecentlyUploadedEnvelope() async throws -> McuMgrResponse {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<McuMgrResponse, Error>) in
+            processRecentlyUploadedEnvelope() { response, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+
+                if let response {
+                    continuation.resume(returning: response)
+                } else {
+                    continuation.resume(throwing: McuMgrResponseParseError.invalidPayload)
+                }
+            }
+        }
+    }
+    
+    // MARK: cleanup()
+    
+    public func cleanup() async throws -> McuMgrResponse {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<McuMgrResponse, Error>) in
+            cleanup { response, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+
+                if let response {
+                    continuation.resume(returning: response)
+                } else {
+                    continuation.resume(throwing: McuMgrResponseParseError.invalidPayload)
+                }
+            }
+        }
+    }
+}
