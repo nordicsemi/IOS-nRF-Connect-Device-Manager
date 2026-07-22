a5d8ce1a017e68ea3c8fc512693f28308d00b1a1
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2026-07-22 11:17:06 +0100

    Extended ImageManager with async variants
    Some testing of this has happened. Some.

---
+++ iOSMcuManagerLibrary/Source/Managers/ImageManager+Async.swift
@@ -0,0 +1,90 @@
+//
+//  ImageManager+Async.swift
+//  iOSMcuManagerLibrary
+//
+//  Created by Dinesh Harjani on 09/07/2026.
+//
+
+import Foundation
+
+// MARK: - ImageManager+Async
+
+@available(iOS 13.0, macCatalyst 13.0, macOS 10.15, *)
+public extension ImageManager {
+    
+    // MARK: list() async
+    
+    public func list() async throws -> McuMgrImageStateResponse {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<McuMgrImageStateResponse, Error>) in
+            list() { response, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                
+                if let response {
+                    continuation.resume(returning: response)
+                } else {
+                    continuation.resume(throwing: McuMgrResponseParseError.invalidPayload)
+                }
+            }
+        }
+    }
+    
+    // MARK: test() async
+    
+    public func test(hash: [UInt8]) async throws -> McuMgrImageStateResponse {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<McuMgrImageStateResponse, Error>) in
+            test(hash: hash) { response, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                
+                if let response {
+                    continuation.resume(returning: response)
+                } else {
+                    continuation.resume(throwing: McuMgrResponseParseError.invalidPayload)
+                }
+            }
+        }
+    }
+    
+    // MARK: confirm() async
+    
+    public func confirm(hash: [UInt8]) async throws -> McuMgrImageStateResponse {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<McuMgrImageStateResponse, Error>) in
+            confirm(hash: hash) { response, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                
+                if let response {
+                    continuation.resume(returning: response)
+                } else {
+                    continuation.resume(throwing: McuMgrResponseParseError.invalidPayload)
+                }
+            }
+        }
+    }
+    
+    // MARK: erase() async throws
+    
+    public func erase(image: Int? = nil, slot: Int? = nil) async throws -> McuMgrResponse {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<McuMgrResponse, Error>) in
+            erase(image: image, slot: slot) { response, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                
+                if let response {
+                    continuation.resume(returning: response)
+                } else {
+                    continuation.resume(throwing: McuMgrResponseParseError.invalidPayload)
+                }
+            }
+        }
+    }
+}
